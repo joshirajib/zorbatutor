0fa41551ad3534fcdd4f7c3b45cce4d6754f102c
joshirajib <joshirajib555@gmail.com>
2020-01-25 15:48:10 -0500

    my first change

--- project1/src/com/zorba/Main.java
+++ project1/src/com/zorba/Main.java
@@ -3,6 +3,14 @@ package com.zorba;
 public class Main {
 
     public static void main(String[] args) {
+
+        Main main = new Main();
+        main.testMethod();
 	System.out.println("Hello this is my first Java Program");
+    }
+
+    private void testMethod() {
+
+        System.out.println("THIS IS TESt METHOD");
     }
 }
